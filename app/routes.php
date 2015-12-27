20497d62f96f96cf348d5793ed1f3aa6ab22d11e
Scholer <scholer_l@live.com>
2015-12-27 20:15:59 +0800

    new templete

--- app/routes.php
+++ app/routes.php
@@ -1,9 +1,9 @@
 <?php
 
 $app->get('/[{name}]', function ($request, $response, $args) {
-    $view = $this->blade->make('index', $args);
     // Render index view
-    $response->getBody()->write($view);
+    $name = $request->getAttribute('name') ?: 'Slim';
+    $response->getBody()->write("<h1>Hello, $name.</h1>");
 });
 
 $app->any('/hello/{action}[/[{args}]]', 'App\Controllers\HelloController');
