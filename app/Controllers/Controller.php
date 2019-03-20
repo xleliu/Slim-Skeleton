2f51d9c0e2f18970df3c6dd6941823b5c6d00f27
刘小乐 <scholer_l@live.com>
2019-03-20 21:32:45 +0800

    remove __invoke

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -10,14 +10,14 @@ class Controller
     /**
      * Container of slim application.
      *
-     * @var Interop\Container\ContainerInterface
+     * @var \Interop\Container\ContainerInterface
      */
     protected $container;
 
     /**
      * Set container as implementation of ContainerInterface.
      *
-     * @param Interop\Container\ContainerInterface $container
+     * @param \Interop\Container\ContainerInterface $container
      */
     public function __construct(ContainerInterface $container)
     {
@@ -34,32 +34,5 @@ class Controller
     public function __get($property)
     {
         return $this->container[$property];
-    }
-
-    /**
-     * Entry of all controllers and actions;
-     * However you can also override it if you want.
-     *
-     * @param \Psr\Http\Message\ServerRequestInterface $req  PSR7 request
-     * @param \Psr\Http\Message\ResponseInterface      $res  PSR7 response
-     * @param array                                    $args Route parameters
-     */
-    public function __invoke($request, $response, $args)
-    {
-        // We would get first argument as action, if it does not existed, set it as index
-        // else we should get the action from router and remove it from arguments.
-        $action = count($args) < 1 ? 'Index' : ucfirst(array_shift($args));
-        $action = strtolower($request->getMethod()) . $action;
-        // Then if the method is existed, we can call it;
-        // Or we should throw \Slim\Exception\NotFoundException.
-        $instance = new static($this->container);
-        if (method_exists($instance, $action)) {
-            // Get response body and write it to response.
-            $content = call_user_func_array([$instance, $action], $args);
-
-            return $response->withJson($content);
-        } else {
-            throw new NotFoundException($request, $response);
-        }
     }
 }
