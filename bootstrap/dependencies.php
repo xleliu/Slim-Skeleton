f61eb7e4fad39d65856753e1e3777911dd6aa7d1
Scholer <scholer_l@live.com>
2019-09-26 19:23:43 +0800

    add monolog

--- bootstrap/dependencies.php
+++ bootstrap/dependencies.php
@@ -11,3 +11,16 @@ $container->singleton('settings', function () {
 
 // Service factory for the ORM
 $container->singleton('db', require __DIR__ . '/databases.php');
+
+// monolog
+$container->bind('logger', function () {
+    $logFile = __DIR__ . '/../storage/logs/app.log';
+
+    $handler = new Monolog\Handler\StreamHandler($logFile, Monolog\Logger::WARNING);
+    $handler->setFormatter(new Monolog\Formatter\LineFormatter(null, null, true, true));
+
+    $logger = new Monolog\Logger('logger');
+    $logger->pushHandler($handler);
+
+    return $logger;
+});
